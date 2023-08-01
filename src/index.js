443feaaa853463cd0bca7975824f0499ae5b0db8
Ivan Rashrahovich <rashragovichivan@gmail.com>
2023-08-01 17:35:19 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n).toString();
+    let result = '';
+    for (let i = 0; i < n.length; i++ ) {
+        result = n[i] + result;
+    }
+
+    return result;
 }
